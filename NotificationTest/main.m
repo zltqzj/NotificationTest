34c1e11822efd9b385e4db022a253bf20d50f322
zltqzj <zltqzj@163.com>
2013-09-12 14:39:21 +0800

    Initial Commit

---
+++ NotificationTest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NotificationTest
+//
+//  Created by Sinosoft on 9/12/13.
+//  Copyright (c) 2013 com.Sinosoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
